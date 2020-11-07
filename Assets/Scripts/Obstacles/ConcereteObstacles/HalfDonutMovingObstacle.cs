45074650defd0ca110bb1993b535e72d0464bfb8
blacqvve <yigitcanoksuz@gmail.com>
2020-11-07 12:51:07 +0300

    new donut obstacle added

---
+++ Assets/Scripts/Obstacles/ConcereteObstacles/HalfDonutMovingObstacle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfDonutMovingObstacle : Obstacle, IDynamicObstacle
+{
+     #region Variables
+     Rigidbody rb;
+
+     [SerializeField]
+     float moveSpeed = 5f, lerpThreshold = 0.1f;
+
+     Vector3 startPosition = Vector3.zero;
+
+     Vector3 targetPosition = Vector3.zero;
+
+
+     #endregion
+
+     #region Engine Methods
+     void Start()
+     {
+          rb = GetComponent<Rigidbody>();
+          startPosition = transform.localPosition;
+          targetPosition = new Vector3(-startPosition.x, startPosition.y, startPosition.z);
+          Movement();
+     }
+     #endregion
+
+
+     #region OtherMethods
+     public void Movement()
+     {
+          StartCoroutine(MoveDonut());
+     }
+
+     private IEnumerator MoveDonut()
+     {
+          while (true)
+          {
+               int interval = UnityEngine.Random.Range(1, 5);
+               yield return new WaitForSecondsRealtime(interval);
+               while (Vector3.Distance(transform.localPosition, targetPosition) > lerpThreshold)
+               {
+                   transform.localPosition=Vector3.Lerp(transform.localPosition, targetPosition, moveSpeed * Time.fixedDeltaTime);
+                    yield return new WaitForFixedUpdate();
+               }
+               while (Vector3.Distance(transform.localPosition, startPosition) > lerpThreshold)
+               {
+                    transform.localPosition = Vector3.Lerp(transform.localPosition,startPosition, moveSpeed * Time.fixedDeltaTime);
+                    yield return new WaitForFixedUpdate();
+               }
+               yield return null;
+          }
+     }
+
+     #endregion
+
+}
---
+++ Assets/Scripts/Obstacles/ConcereteObstacles/HalfDonutRotatingObstacle.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfDonutRotatingObstacle : Obstacle, IDynamicObstacle
+{
+     #region Variables
+     Rigidbody rb;
+
+     [SerializeField]
+     float rotationSpeed = 5f;
+     [SerializeField]
+     float lerpThreshold = 1f;
+     [SerializeField]
+     float rotateAmount = 90f;
+     #endregion
+
+
+     private void Start()
+     {
+          rb = GetComponent<Rigidbody>();
+          Movement();
+     }
+     public void Movement()
+     {
+          StartCoroutine(RotateDonut());
+     }
+     IEnumerator RotateDonut()
+     {
+          while (true)
+          {
+               int interval = UnityEngine.Random.Range(1, 5);
+               Quaternion targetRotation = Quaternion.Euler(new Vector3(rotateAmount, 0, 0));
+               yield return new WaitForSecondsRealtime(interval);
+               while (Quaternion.Angle(transform.localRotation, targetRotation)>lerpThreshold)
+               {
+                    transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+                    yield return new WaitForFixedUpdate();
+               }
+               targetRotation = Quaternion.Euler(Vector3.zero);
+               while (Quaternion.Angle(transform.localRotation, targetRotation)> lerpThreshold)
+               {
+                    transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+                    yield return new WaitForFixedUpdate();
+               }
+               yield return null; 
+          }
+     }
+
+}
