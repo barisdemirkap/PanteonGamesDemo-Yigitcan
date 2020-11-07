e5cf2c70e49a8c0215690a7ba84164bd56166599
blacqvve <yigitcanoksuz@gmail.com>
2020-11-07 12:46:17 +0300

    Generic state machine

---
+++ Assets/Scripts/Helpers/StateMachine/State.cs
@@ -0,0 +1,12 @@
+﻿public abstract class State<T>
+{
+     protected T context;
+
+     protected State(T context)
+     {
+          this.context = context;
+     }
+     public abstract void Update();
+     public virtual void OnStateEnter() { }
+     public virtual void OnStateExit() { }
+}
---
+++ Assets/Scripts/Helpers/StateMachine/StateMachine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine<T>
+{
+     public Dictionary<int, State<T>> StateCache { get; protected set; }
+
+     public State<T> currentState;
+
+     public StateMachine()
+     {
+          StateCache = new Dictionary<int, State<T>>();
+     }
+     public void AddToCache(int key, State<T> state)
+     {
+          if (StateCache.ContainsKey(key))
+               return;
+          StateCache.Add(key, state);
+     }
+     public void RemoveFromCache(int key)
+     {
+          if (StateCache.ContainsKey(key))
+               StateCache.Remove(key);
+
+     }
+     public void SetState(State<T> newState)
+     {
+          currentState?.OnStateExit();
+
+          currentState = newState;
+
+          currentState?.OnStateEnter();
+     }
+}
