ad11e087e4304efdc6d281bfb2216bf459fae48e
blacqvve <yigitcanoksuz@gmail.com>
2020-11-08 19:53:20 +0300

    victory and defeat motions, ui textures, new obstacle textures, new obstacle rotating wall, new platform jumppad, progress bar script and prefab changes

---
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ProgressBar : MonoBehaviour
+{
+     private Text progressText;
+     private Image progressImage;
+    void Start()
+    {
+          progressImage = GetComponent<Image>();
+          progressText = GetComponentInChildren<Text>();
+    }
+
+     public void UpdateProgress(float value)
+     {
+          progressImage.fillAmount = value;
+          progressText.text = value * 100 + "%";
+     }
+}
